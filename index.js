50b8e073b618ecf9d7c95928aea06c56d459365f
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2025-06-08 12:19:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+console.log(`
+=============================
+  ATUL PATIDAR
+  FULL STACK DEVELOPER
+=============================
+
+📍 Indore, Madhya Pradesh
+📞 +91 8964828585
+📧 Atulpatidar1709@gmail.com
+
+🌐 LinkedIn: https://www.linkedin.com/in/atulpatidar09/
+🌐 GitHub: https://github.com/AtulPatidar1709
+🌐 TUF Profile: https://takeuforward.org/profile/Atul%20Baba
+
+🚀 Skills:
+- Languages: JavaScript, Java, TypeScript.
+- Frontend: React.js, Next.js, Redux, HTML, CSS, Tailwind CSS, Shadcn, Material UI.
+- Backend: Node.js, Express.js, Spring Boot, REST API, JWT.
+- Database: MongoDB, MySQL, Mongoose.
+- Tools: Git, GitHub, Postman, VS Code, Intelige, Docker.
+- Other: Stripe Integration, Clerk Auth, Agile.
+
+📚 Projects:
+1. TourTrove – Full-stack travel booking platform with Clerk auth & listings
+   🔗 Demo: https://tour-trove-eight.vercel.app/
+   🔗 Code: https://github.com/AtulPatidar1709/TourTrove
+
+2. VisionSpace – Real-time collaborative workspace with WebSocket
+   🔗 Demo: https://visonspace.vercel.app/
+   🔗 Code: https://github.com/AtulPatidar1709/visonspace
+
+3. E-commerce Platform – Admin dashboard, Stripe, custom APIs
+   🔗 Demo: https://ecommerceshoes.vercel.app/
+   🔗 Code: https://github.com/AtulPatidar1709/Ecomerse_NextJs
+
+4. Journal App – Spring Boot & React.js with secure CRUD and JWT
+   🔗 Code: https://github.com/AtulPatidar1709/journalApp
+
+🎓 Education:
+- B.Tech in Computer Science, IES IPS Academy, Indore (2018–2021) | CGPA: 7.65
+- Diploma in Computer Science, S.V. Polytechnic College, Bhopal (2015–2018) | CGPA: 6.59
+
+📜 Certifications:
+- Full SDE & DSA Series – TakeUForward (2025)
+- Complete JavaScript Course – Udemy (2023)
+- JavaScript Algorithms – FreeCodeCamp (2022)
+- DBMS – NPTEL, IIT Kharagpur (2021)
+
+=============================
+`);
